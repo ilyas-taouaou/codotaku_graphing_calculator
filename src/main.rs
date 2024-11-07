54d8eaa081f3857447e698b7403ae0f8c074be3a
ilyas-taouaou <IlyasTaouaou@my.uopeople.edu>
2024-11-07 08:40:20 +0100

    clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Mutex;
+
 use bevy::{
     pbr::wireframe::{Wireframe, WireframePlugin},
     prelude::*,
@@ -23,15 +25,14 @@ fn main() {
         .add_plugins(WireframePlugin)
         .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, configuration_changed)
+        .add_systems(Update, update_graphs)
         .run();
 }
 
 struct Graph {
     positions: Vec<Vec3>,
-    normals: Vec<[f32; 3]>,
-    uvs: Vec<[f32; 2]>,
     indices: Vec<u32>,
+    context: HashMapContext,
 }
 
 impl Graph {
@@ -39,11 +40,9 @@ impl Graph {
         let z_vertex_count = subdivisions + 2;
         let x_vertex_count = subdivisions + 2;
         let num_vertices = (z_vertex_count * x_vertex_count) as usize;
-        let num_indices = ((z_vertex_count - 1) * (x_vertex_count - 1) * 6) as usize;
+        let num_indices = ((z_vertex_count - 1) * x_vertex_count * 2) as usize;
 
         let mut positions: Vec<Vec3> = Vec::with_capacity(num_vertices);
-        let mut normals: Vec<[f32; 3]> = Vec::with_capacity(num_vertices);
-        let mut uvs: Vec<[f32; 2]> = Vec::with_capacity(num_vertices);
         let mut indices: Vec<u32> = Vec::with_capacity(num_indices);
 
         let rotation = Quat::from_rotation_arc(Vec3::Y, *normal);
@@ -55,46 +54,54 @@ impl Graph {
                 let tz = z as f32 / (z_vertex_count - 1) as f32;
                 let pos = rotation * Vec3::new((-0.5 + tx) * size.x, 0.0, (-0.5 + tz) * size.y);
                 positions.push(pos);
-                normals.push(normal.to_array());
-                uvs.push([tx, tz]);
             }
         }
 
         for z in 0..z_vertex_count - 1 {
-            for x in 0..x_vertex_count - 1 {
-                let quad = z * x_vertex_count + x;
-                indices.push(quad + x_vertex_count + 1);
-                indices.push(quad + 1);
-                indices.push(quad + x_vertex_count);
-                indices.push(quad);
-                indices.push(quad + x_vertex_count);
-                indices.push(quad + 1);
+            if z % 2 == 0 {
+                for x in 0..x_vertex_count {
+                    indices.push(z * x_vertex_count + x);
+                    indices.push((z + 1) * x_vertex_count + x);
+                }
+            } else {
+                for x in (0..x_vertex_count).rev() {
+                    indices.push((z + 1) * x_vertex_count + x);
+                    indices.push(z * x_vertex_count + x);
+                }
             }
         }
 
+        let context = evalexpr::context_map! {
+            "cos" => Function::new(|argument| {
+                let number: f64 = argument.as_number()?;
+                Ok(Value::Float(number.cos()))
+            }),
+            "sin" => Function::new(|argument| {
+                let number: f64 = argument.as_number()?;
+                Ok(Value::Float(number.sin()))
+            }),
+        }
+        .unwrap();
+
         Graph {
             positions,
-            normals,
-            uvs,
             indices,
+            context,
         }
     }
 
-    fn update(
-        &mut self,
-        expression: &Node,
-        context: &mut HashMapContext,
-        delta_time: f32,
-    ) -> Result<(), EvalexprError> {
+    fn update(&mut self, expression: &Node, delta_time: f32, t: f64) -> Result<(), EvalexprError> {
+        self.context
+            .set_value("t".into(), Value::from_float(t))
+            .unwrap();
         for vertex in self.positions.iter_mut() {
-            context
+            self.context
                 .set_value("x".into(), Value::from_float(vertex.x as f64))
                 .unwrap();
-            context
+            self.context
                 .set_value("z".into(), Value::from_float(vertex.z as f64))
                 .unwrap();
-            let target = expression.eval_number_with_context(context)? as f32;
-            // lerp
+            let target = expression.eval_number_with_context(&mut self.context)? as f32;
             let lerp_duration = 0.1;
             vertex.y = vertex.y.lerp(target, delta_time / lerp_duration);
         }
@@ -103,17 +110,18 @@ impl Graph {
 
     fn mesh(&self) -> Mesh {
         Mesh::new(
-            PrimitiveTopology::TriangleList,
+            PrimitiveTopology::TriangleStrip,
             RenderAssetUsages::default(),
         )
         .with_inserted_indices(Indices::U32(self.indices.clone()))
         .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, self.positions.clone())
-        .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, self.normals.clone())
-        .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, self.uvs.clone())
     }
 }
 
 use bevy_inspector_egui::prelude::ReflectInspectorOptions;
+use rayon::iter::{
+    IndexedParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator,
+};
 
 #[derive(Reflect, Resource, InspectorOptions)]
 #[reflect(Resource, InspectorOptions)]
@@ -124,45 +132,47 @@ struct Configuration {
 impl Default for Configuration {
     fn default() -> Self {
         Self {
-            expressions: vec!["0.5*cos(x*6+t)".to_string(), "0.5*sin(x*6+t)".to_string()],
+            // beautiful 3d graphs that take x, z and t (time) as input
+            expressions: vec![
+                "(x * 2.0) * (z * 2.0) * sin(t * 2.0)".to_string(),
+                "cos(x * 2.0) * cos(z * 2.0) * sin(t * 2.0)".to_string(),
+            ],
         }
     }
 }
 
-fn configuration_changed(
+fn update_graphs(
     configuration: Res<Configuration>,
     mut graphs_resource: ResMut<Graphs>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut commands: Commands,
     time: Res<Time>,
 ) {
-    // if configuration.is_changed() {
-    let mut context = std::mem::take(&mut graphs_resource.context);
+    let t = time.elapsed_seconds_f64();
+    let dt = time.delta_seconds();
 
-    context
-        .set_value(
-            "t".into(),
-            Value::from_float(time.elapsed_seconds_f64() as f64),
-        )
-        .unwrap();
+    let queued_meshes = Mutex::new(Vec::new());
 
-    for (expression, (graph, entity)) in configuration
+    configuration
         .expressions
-        .iter()
-        .zip(graphs_resource.graphs.iter_mut())
-    {
-        if let Ok(expression) = evalexpr::build_operator_tree(expression) {
-            if graph
-                .update(&expression, &mut context, time.delta_seconds())
-                .is_ok()
-            {
-                let mesh = meshes.add(graph.mesh());
-                commands.get_entity(entity.clone()).unwrap().insert(mesh);
+        .par_iter()
+        .zip(graphs_resource.graphs.par_iter_mut())
+        .for_each(|(expression, (graph, entity))| {
+            if let Ok(expression) = evalexpr::build_operator_tree(expression) {
+                if graph.update(&expression, dt, t).is_ok() {
+                    queued_meshes.lock().unwrap().push((graph.mesh(), *entity));
+                }
             }
-        }
+        });
+
+    for (mesh, entity) in queued_meshes.lock().unwrap().iter() {
+        commands.entity(*entity).insert(PbrBundle {
+            mesh: meshes.add(mesh.clone()),
+            material: graphs_resource.material.clone(),
+            ..default()
+        });
     }
 
-    // generate new graphs if needed
     for expression in configuration
         .expressions
         .iter()
@@ -170,10 +180,7 @@ fn configuration_changed(
     {
         if let Ok(expression) = evalexpr::build_operator_tree(expression) {
             let mut graph = Graph::new(64, Dir3::Y, Vec2::splat(0.5));
-            if graph
-                .update(&expression, &mut context, time.delta_seconds())
-                .is_ok()
-            {
+            if graph.update(&expression, dt, t).is_ok() {
                 let mesh = meshes.add(graph.mesh());
                 let entity = commands.spawn((
                     PbrBundle {
@@ -188,35 +195,32 @@ fn configuration_changed(
         }
     }
 
-    graphs_resource.context = context;
-    // }
+    if configuration.expressions.len() < graphs_resource.graphs.len() {
+        for (_, entity) in graphs_resource
+            .graphs
+            .iter()
+            .skip(configuration.expressions.len())
+        {
+            commands.entity(*entity).despawn();
+        }
+        graphs_resource
+            .graphs
+            .truncate(configuration.expressions.len());
+    }
 }
 
 #[derive(Resource)]
 struct Graphs {
     graphs: Vec<(Graph, Entity)>,
-    context: HashMapContext,
     material: Handle<StandardMaterial>,
 }
 
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<StandardMaterial>>) {
-    let mut mat: StandardMaterial = Color::srgba(1.0, 1.0, 1.0, 0.5).into();
+    let mut mat: StandardMaterial = Color::WHITE.into();
     mat.cull_mode = None;
     mat.alpha_mode = AlphaMode::Blend;
     mat.unlit = true;
     let white_material = materials.add(mat);
-
-    let context = evalexpr::context_map! {
-        "cos" => Function::new(|argument| {
-            let number: f64 = argument.as_number()?;
-            Ok(Value::Float(number.cos()))
-        }),
-        "sin" => Function::new(|argument| {
-            let number: f64 = argument.as_number()?;
-            Ok(Value::Float(number.sin()))
-        }),
-    }
-    .unwrap();
 
     let _camera = commands.spawn((
         Camera3dBundle {
@@ -229,7 +233,6 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<StandardMaterial>>
     let graphs = vec![];
     commands.insert_resource(Graphs {
         graphs,
-        context,
         material: white_material,
     });
 }
